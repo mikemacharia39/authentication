5c8f34e4b7257ad24deb8899b4111a7401e9a05b
mikemacharia39 <mikemacharia39@gmail.com>
2025-12-30 21:39:47 +0300

    Minor improvements

--- src/main/java/com/mikehenry/authentication/domain/repository/LoginAttemptRepository.java
+++ src/main/java/com/mikehenry/authentication/domain/repository/LoginAttemptRepository.java
@@ -1,10 +1,13 @@
 package com.mikehenry.authentication.domain.repository;
 
 import com.mikehenry.authentication.domain.entity.LoginAttempt;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LoginAttemptRepository extends JpaRepository<LoginAttempt, Long>, JpaSpecificationExecutor<LoginAttempt> {
+    Page<LoginAttempt> findByEmailOrderByAttemptedAtDesc(String email, Pageable pageable);
 }
