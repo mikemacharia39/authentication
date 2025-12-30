6a7d8d5ac8327aaf58148acb8f94d89bc8ffe15c
mikemacharia39 <mikemacharia39@gmail.com>
2025-12-30 20:56:13 +0300

    login impl

--- src/main/java/com/mikehenry/authentication/domain/service/LoginService.java
+++ src/main/java/com/mikehenry/authentication/domain/service/LoginService.java
@@ -2,16 +2,46 @@ package com.mikehenry.authentication.domain.service;
 
 import com.mikehenry.authentication.api.dto.LoginRequest;
 import com.mikehenry.authentication.api.dto.LoginResponse;
+import com.mikehenry.authentication.domain.entity.LoginAttempt;
+import com.mikehenry.authentication.domain.repository.LoginAttemptRepository;
+import com.mikehenry.authentication.domain.util.JwtHelper;
 import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.NonNull;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class LoginService {
     private final AuthenticationManager authenticationManager;
+    private final LoginAttemptRepository loginAttemptRepository;
 
-    public LoginResponse login(final LoginRequest loginRequest) {
+    public LoginResponse login(@NonNull final LoginRequest loginRequest) {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.email(), loginRequest.password()));
+        } catch (BadCredentialsException e) {
+            addLoginAttempt(loginRequest.email(), false);
+            throw e;
+        }
 
+        final String token = JwtHelper.generateToken(loginRequest.email());
+        addLoginAttempt(loginRequest.email(), true);
+
+        return LoginResponse.builder()
+                .accessToken(token)
+                .tokenType("Bearer")
+                .build();
+    }
+
+    @Transactional
+    public void addLoginAttempt(final String email, final boolean success) {
+        LoginAttempt loginAttempt = LoginAttempt.builder()
+                .email(email)
+                .successful(success)
+                .build();
+        loginAttemptRepository.save(loginAttempt);
     }
 }
