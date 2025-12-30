3ee3b74488f7addb5797a63f942a04be18123dcf
mikemacharia39 <mikemacharia39@gmail.com>
2025-12-30 20:56:12 +0300

    Login response

--- src/main/java/com/mikehenry/authentication/api/dto/LoginResponse.java
+++ src/main/java/com/mikehenry/authentication/api/dto/LoginResponse.java
@@ -1,7 +1,9 @@
 package com.mikehenry.authentication.api.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
 
+@Builder
 public record LoginResponse(
         @Schema(description = "JWT token")
         String accessToken,
