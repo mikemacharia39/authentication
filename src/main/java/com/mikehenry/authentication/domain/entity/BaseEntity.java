1a7458a2ed5bc8bae4487c077226c3680d5fddf2
mikemacharia39 <mikemacharia39@gmail.com>
2025-12-30 19:18:34 +0300

    New class

---
+++ src/main/java/com/mikehenry/authentication/domain/entity/BaseEntity.java
@@ -0,0 +1,50 @@
+package com.mikehenry.authentication.domain.entity;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+@SuperBuilder(toBuilder = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@MappedSuperclass
+public abstract class BaseEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, updatable = false)
+    private Long id;
+
+    @CreatedDate
+    @Column(name = "date_created", nullable = false, updatable = false, columnDefinition = "DATETIME")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
+    private Instant dateCreated;
+
+    @LastModifiedDate
+    @Column(name = "date_modified", columnDefinition = "DATETIME")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
+    private Instant dateModified;
+
+    @PrePersist
+    @PreUpdate
+    void prePersist() {
+        if (this.dateCreated == null) {
+            this.dateCreated = Instant.now();
+        }
+        this.dateModified = Instant.now();
+    }
+}
