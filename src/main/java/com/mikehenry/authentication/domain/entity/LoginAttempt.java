abcf6489bf9a34ac5e4e46507793533dcb9591e6
mikemacharia39 <mikemacharia39@gmail.com>
2025-12-30 21:39:47 +0300

    Minor improvements

--- src/main/java/com/mikehenry/authentication/domain/entity/LoginAttempt.java
+++ src/main/java/com/mikehenry/authentication/domain/entity/LoginAttempt.java
@@ -10,10 +10,12 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
+import lombok.Getter;
 import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.CreatedDate;
 
 @SuperBuilder
+@Getter
 @Entity
 @Table(name = "login_attempt")
 public class LoginAttempt {
