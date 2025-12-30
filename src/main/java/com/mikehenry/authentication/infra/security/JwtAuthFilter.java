97a8ec1efa6f2ae8f4c5e331998a7da4e27074b5
mikemacharia39 <mikemacharia39@gmail.com>
2025-12-30 20:53:52 +0300

    jwt auth filter class

---
+++ src/main/java/com/mikehenry/authentication/infra/security/JwtAuthFilter.java
@@ -0,0 +1,74 @@
+package com.mikehenry.authentication.infra.security;
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mikehenry.authentication.api.dto.Problem;
+import com.mikehenry.authentication.domain.service.UserDetailsServiceImpl;
+import com.mikehenry.authentication.domain.util.JwtHelper;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@RequiredArgsConstructor
+@Component
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private final UserDetailsServiceImpl userDetailsService;
+    private final ObjectMapper objectMapper;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        try {
+            String authHeader = request.getHeader("Authorization");
+
+            String token = null;
+            String username = null;
+            if (authHeader != null && authHeader.startsWith("Bearer ")) {
+                token = authHeader.substring(7);
+                username = JwtHelper.extractUsername(token);
+            }
+
+            // The expectation is any request having access token = null, is either a login, reset-password or forgot-password request.
+            // Therefore, such requests will be passed to next filter in the filter chain.
+            if (token == null) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+
+            if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+                if (JwtHelper.validateToken(token, userDetails)) {
+                    UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, null);
+                    authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                }
+            }
+
+            filterChain.doFilter(request, response);
+        } catch (AccessDeniedException e) {
+            Problem errorResponse = new Problem(HttpServletResponse.SC_FORBIDDEN, e.getMessage());
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            response.getWriter().write(toJson(errorResponse));
+        }
+    }
+
+    private String toJson(Problem response) {
+        try {
+            return objectMapper.writeValueAsString(response);
+        } catch (JsonProcessingException e) {
+            return "{\"errorCode\":" + response.errorCode() + ",\"message\":\"" + response.message() + "\"}";
+        }
+    }
+}
