5bff2c556d3268430f1ecf6b11be514aa242ffa3
mikemacharia39 <mikemacharia39@gmail.com>
2025-12-30 19:18:35 +0300

    New class

---
+++ src/main/java/com/mikehenry/authentication/domain/service/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.mikehenry.authentication.domain.service;
+
+import com.mikehenry.authentication.domain.entity.User;
+import com.mikehenry.authentication.domain.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.NonNull;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public @NonNull UserDetails loadUserByUsername(@NonNull String username) throws UsernameNotFoundException {
+
+        final User user = userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + username));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .build();
+    }
+}
