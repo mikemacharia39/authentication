7d74bd79a8399a330dd9d54530d73a8ee65ddabc
mikemacharia39 <mikemacharia39@gmail.com>
2025-12-31 20:54:21 +0300

    Use date created

--- src/main/java/com/mikehenry/authentication/domain/repository/LoginAttemptRepository.java
+++ src/main/java/com/mikehenry/authentication/domain/repository/LoginAttemptRepository.java
@@ -9,5 +9,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LoginAttemptRepository extends JpaRepository<LoginAttempt, Long>, JpaSpecificationExecutor<LoginAttempt> {
-    Page<LoginAttempt> findByEmailOrderByAttemptedAtDesc(String email, Pageable pageable);
+    Page<LoginAttempt> findByEmailOrderByIdDesc(String email, Pageable pageable);
 }
