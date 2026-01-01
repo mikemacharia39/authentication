64a346c5bcc529132642ed705324e923f1e4811a
mikemacharia39 <mikemacharia39@gmail.com>
2026-01-01 14:49:51 +0300

    Add no arg constructor

--- src/main/java/com/mikehenry/authentication/domain/entity/UserCode.java
+++ src/main/java/com/mikehenry/authentication/domain/entity/UserCode.java
@@ -4,12 +4,16 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @SuperBuilder(toBuilder = true)
 @Getter
 @Entity
