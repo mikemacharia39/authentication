6ccabb946d4697001b012eb56138e34dfcf1ef4c
mikemacharia39 <mikemacharia39@gmail.com>
2026-01-02 13:42:02 +0300

    enums roles

---
+++ src/main/java/com/mikehenry/authentication/domain/role/Role.java
@@ -0,0 +1,10 @@
+package com.mikehenry.authentication.domain.role;
+
+// roles in a loan management system
+public enum Role {
+    ADMIN,
+    USER,
+    LOAN_UNDERWRITER,
+    LOAN_UNDERWRITER_MANAGER,
+    AUDITOR
+}
